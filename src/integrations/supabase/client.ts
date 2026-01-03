8a46407bb63eaf8021dd88cfb46c8c95a07112e2
MH Shuvo <mhshuvoalways@gmail.com>
2026-01-03 13:08:30 +0600

    updated

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://bbdclvybiahlrrxazmlo.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiZGNsdnliaWFobHJyeGF6bWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxODIxNDQsImV4cCI6MjA3NTc1ODE0NH0.U7V2ZAFaANppBAsoTdl5VZLnpKslxpgMkiNZiLS9Nkc";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
